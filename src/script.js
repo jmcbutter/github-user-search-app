c5ebf9d1895570d694b6cce7d61858d48aeb6446
Jordan Butterfield <jmcbutter@gmail.com>
2022-09-30 06:57:58 -0500

    Refactor Form controls and Theme switches in JavaScript

--- src/script.js
+++ src/script.js
@@ -1,71 +1,99 @@
 "use strict";
-let user = "octocat";
+let user = "tj";
 fetchUser(user);
 
-/*                        FORM CONTROL                        */
+/*----------------------------------------------------------------------------*/
+/*                               FORM CONTROLS                              
+/*----------------------------------------------------------------------------*/
 
-let form = document.getElementById("user-search");
-form.addEventListener("submit", (e) => e.preventDefault())
+const FORM = document.getElementById("user-search");
+const SEARCH_BUTTON = document.getElementById("search-button");
+const SEARCH_INPUT = document.getElementById("search");
+const SEARCH_LABEL = document.getElementById("search-label");
 
-let searchButton = document.getElementById("search-button");
-searchButton.addEventListener("click", onSearchButtonClicked);
+FORM.addEventListener("submit", (e) => e.preventDefault())
 
-let searchInput = document.getElementById("search");
-searchInput.addEventListener("focusin", (e) => {
-  document.getElementById("search-label").classList.add("isHidden");
+SEARCH_INPUT.addEventListener("focusin", (e) => {
+  SEARCH_LABEL.classList.add("isHidden");
   e.target.value = "";
 });
 
-searchInput.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    e.target.blur();
-  }
+SEARCH_INPUT.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") e.target.blur() 
 });
 
-function onSearchButtonClicked(e) {
-  let user = searchInput.value;
-  if (!user) {
-    document.getElementById("search-label").classList.remove("isHidden");
-  } else {
-    fetchUser(user);
-  }
-}
+SEARCH_BUTTON.addEventListener("click", e => {
+  let user = SEARCH_INPUT.value;
+  user ? fetchUser(user) : SEARCH_LABEL.classList.remove("isHidden");
+});
 
 
-/*                            API FETCH                               */
+
+/*----------------------------------------------------------------------------*/
+/*                                 API FETCH                                  
+/*----------------------------------------------------------------------------*/
+
+//TODO Make Website, Twitter, and Company Information all links. Company should link to company's page on github
+//TODO Remove @ symbol from company link for link URL (ex: @github ==> https://github.com/github)
+//TODO Show User Tag without @ Symbol in UserName field if name is empty
+
 function fetchUser(user) {
   fetch(`https://api.github.com/users/${user}`)
-    .then(response => {
-      console.log(response);
-      if (!response.ok) {
-        document.getElementById("search-label").classList.remove("isHidden");
-        throw Error(response.statusText);
-      } else {
-        return response.json()
-      }
-    })
-    .then(data => updateUser(data))
+    .then(response => handleFetchResponse(response))
+    .then(data => updateUserFields(data))
     .catch(error => console.log(error));
 }
 
-function update(element, value, href=null) {
-  let el = document.querySelector(`[data-js="${element}"]`);
+function handleFetchResponse(response) {
+  if (!response.ok) {
+    document.getElementById("search-label").classList.remove("isHidden");
+    throw Error(response.statusText);
+  } else {
+    return response.json()
+  }
+}
+
+function updateUserFields(data) {
+  const USER_FIELDS = ["avatar", "name", "tag", "joinDate", "bio", "repos", 
+                     "followers", "following", "location", "twitter", "website", 
+                     "company"]
+
+  USER_FIELDS.forEach(field => update(field, data));
+}
+
+function update(element, data) {
+  let el = document.querySelector(`[data-user="${element}"]`);
+  let value;
 
   switch(element) {
-    case "userAvatar":
+    case "avatar":
+      value = data.avatar_url;
       el.setAttribute("src", value);
       break;
-    case "userTag":
+    case "name":
+      value = data.name;
+      if(value || value === 0) {
+        el.textContent = value;
+        el.parentNode.classList.remove("grayed-out");
+      } else {
+        el.textContent = "No Name Available";
+        el.parentNode.classList.add("grayed-out");
+      }
+      break;
+    case "tag":
+      value = data.login;
       el.textContent = "@" + value;
       break;
-    case "userBio":
+    case "bio":
+      value = data.bio;
       if (value == null) {
         el.textContent = "This profile has no bio";
       } else {
         el.textContent = value;
       }
       break;
-    case "userJoinDate": 
+    case "joinDate": 
+      value = data.created_at;
       let date = new Date(value);
       let formatter = new Intl.DateTimeFormat('en-GB', {
         day: 'numeric', 
@@ -74,20 +102,77 @@ function update(element, value, href=null) {
 
       el.textContent = `Joined ${formatter.format(date)}`
       break;
-    case "userName":
+    case "repos":
+      value = data.public_repos;
       if(value || value === 0) {
-        el.textContent = value;
         el.parentNode.classList.remove("grayed-out");
+        el.textContent = value;
       } else {
-        //TODO Show User Tag without @ Symbol in UserName field if name is empty
-        el.textContent = "No Name Available";
+        el.textContent = "Not Available";
         el.parentNode.classList.add("grayed-out");
       }
       break;
-    case "userWebsite":
-      //TODO Make Website, Twitter, and Company Information all links. Company should link to company's page on github
-      //TODO Remove @ symbol from company link for link URL (ex: @github ==> https://github.com/github)
-      el.setAttribute("href", href);
+    case "followers":
+      value = data.followers;
+      if(value || value === 0) {
+        el.parentNode.classList.remove("grayed-out");
+        el.textContent = value;
+      } else {
+        el.textContent = "Not Available";
+        el.parentNode.classList.add("grayed-out");
+      }
+      break;
+    case "following":
+      value = data.following;
+      if(value || value === 0) {
+        el.parentNode.classList.remove("grayed-out");
+        el.textContent = value;
+      } else {
+        el.textContent = "Not Available";
+        el.parentNode.classList.add("grayed-out");
+      }
+      break;
+    case "location":
+      value = data.location;
+      if(value || value === 0) {
+        el.parentNode.classList.remove("grayed-out");
+        el.textContent = value;
+      } else {
+        el.textContent = "Not Available";
+        el.parentNode.classList.add("grayed-out");
+      }
+      break;
+    case "twitter":
+      value = data.twitter_username;
+      if(value || value === 0) {
+        el.parentNode.classList.remove("grayed-out");
+        el.textContent = value;
+      } else {
+        el.textContent = "Not Available";
+        el.parentNode.classList.add("grayed-out");
+      }
+      break;
+    case "website":
+      value = data.blog;
+      el.setAttribute("href", value);
+      if(value || value === 0) {
+        el.parentNode.classList.remove("grayed-out");
+        el.textContent = value;
+      } else {
+        el.textContent = "Not Available";
+        el.parentNode.classList.add("grayed-out");
+      }
+      break;
+    case "company":
+      value = data.company;
+      if(value || value === 0) {
+        el.parentNode.classList.remove("grayed-out");
+        el.textContent = value;
+      } else {
+        el.textContent = "Not Available";
+        el.parentNode.classList.add("grayed-out");
+      }
+      break;
     default:
       if(value || value === 0) {
         el.parentNode.classList.remove("grayed-out");
@@ -100,35 +185,19 @@ function update(element, value, href=null) {
   }
 }
 
-function updateUser(data) {
-  update("userAvatar", data.avatar_url);
-  update("userName", data.name);
-  update("userTag", data.login);
-  update("userBio", data.bio);
-  update("userRepos", data.public_repos);
-  update("userFollowers", data.followers);
-  update("userFollowing", data.following);
-  update("userLocation", data.location);
-  update("userTwitter", data.twitter_username);
-  update("userWebsite", data.blog, data.blog);
-  update("userCompany", data.company);
-  update("userJoinDate", data.created_at);
+
+/*----------------------------------------------------------------------------*/
+/*                            COLOR THEME SWITCH                              */
+/*----------------------------------------------------------------------------*/
+
+const lightThemeToggleSwitch = document.querySelector(`[data-switch="light"]`);
+const darkThemeToggleSwitch = document.querySelector(`[data-switch="dark"]`);
+
+lightThemeToggleSwitch.addEventListener("click", () => changeThemeTo("light"))
+darkThemeToggleSwitch.addEventListener("click", () => changeThemeTo("dark"))
+
+function changeThemeTo(themeName) {
+  lightThemeToggleSwitch.classList.toggle("isHidden");
+  darkThemeToggleSwitch.classList.toggle("isHidden");
+  document.body.setAttribute("data-theme", themeName);
 }
-
-/*                          COLOR THEME SWITCH                              */
-let lightThemeToggleSwitch = document.querySelector(`[data-switch="light"]`);
-let darkThemeToggleSwitch = document.querySelector(`[data-switch="dark"]`);
-
-//TODO Set color scheme using prefers-color-scheme in CSS
-
-lightThemeToggleSwitch.addEventListener("click", (e) => {
-  lightThemeToggleSwitch.classList.toggle("isHidden");
-  darkThemeToggleSwitch.classList.toggle("isHidden");
-  document.body.setAttribute("data-theme", "default");
-})
-
-darkThemeToggleSwitch.addEventListener("click", (e) => {
-  lightThemeToggleSwitch.classList.toggle("isHidden");
-  darkThemeToggleSwitch.classList.toggle("isHidden");
-  document.body.setAttribute("data-theme", "dark");
-})
